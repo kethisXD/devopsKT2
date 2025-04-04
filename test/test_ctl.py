05df425567902ec971fe77665a1aab068e5334ac
xxx <adsjasdfkjl@google.com>
2025-04-04 05:05:09 +0300

    fix: неправильный путь модуля ctl

--- test/test_ctl.py
+++ test/test_ctl.py
@@ -5,21 +5,21 @@ import sys
 class TestCalcCTL(unittest.TestCase):
     def test_cli_add(self):
         result = subprocess.run(
-            [sys.executable, '-m', 'src.cli', 'add', '5', '3'],
+            [sys.executable, '-m', 'app_ctl.calctl', 'add', '5', '3'],
             capture_output=True, text=True
         )
         self.assertEqual(result.stdout.strip(), '8.0')
         
     def test_cli_subtract(self):
         result = subprocess.run(
-            [sys.executable, '-m', 'src.cli', 'subtract', '10', '4'],
+            [sys.executable, '-m', 'app_ctl.calctl', 'subtract', '10', '4'],
             capture_output=True, text=True
         )
         self.assertEqual(result.stdout.strip(), '6.0')
         
     def test_cli_error(self):
         result = subprocess.run(
-            [sys.executable, '-m', 'src.cli', 'divide', '5', '0'],
+            [sys.executable, '-m', 'app_ctl.calctl', 'divide', '5', '0'],
             capture_output=True, text=True
         )
         self.assertIn('Error', result.stderr)
